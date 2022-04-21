206a85a5a2f5a831372c21dbc0c623688c3fe787
work <9779930@mail.ru>
2022-04-21 13:31:59 +0300

    ready to send. made on work

--- project_storage.py
+++ project_storage.py
@@ -1,40 +1,75 @@
 import random
-import window
+from window import Window
+
+
+class MyTypeError(Exception):
+    pass
+
+
+class MyQuantityError(Exception):
+    pass
 
 
 class Storage:
-    storage = {}
+    def __init__(self):
+        self.storage = {'Printer': [], 'Scanner': [], 'Copier': []}
 
-    def accept(self, equipment_type):
+    # добавляем случайную технику
+    def random_accept(self, equipment_type):
         match equipment_type:
             case 'Printer':
                 func = self.random_printer()
-            case 'Scaner':
-                func = self.random_printer()
+            case 'Scanner':
+                func = self.random_scanner()
             case 'Copier':
-                func = self.random_printer()
-        if equipment_type not in self.storage:
-            self.storage[equipment_type] = []
-            self.storage[equipment_type].append(self.random_printer())
-        else:
-            self.storage[equipment_type].append(self.random_printer())
+                func = self.random_copier()
+        self.storage[equipment_type].append(func)
 
     def __str__(self):
         storage_string = ''
         for key, value in self.storage.items():
-            storage_string += f'{key}:\n'
+            storage_string += f'{key}s:\n'
+            count = 0
             for item in value:
-                storage_string += f'{item.model}({item.serial_number}), '
-            storage_string.strip(', ')
-            storage_string += '\n'
+                count += 1
+                storage_string += f'{count}. {item.model}({item.serial_number})\n'
         return storage_string
 
+    # собираем строку со всем, что есть на складе
     def count(self):
-        count_str = 'На складе\n'
+        count_str = 'In storage\n'
         for key, value in self.storage.items():
             count_str += f'{len(value)} {key}s\n'
         return count_str
 
+    # перемещение техники
+    def move_to(self, target_obj, *equipment):
+        printers, scanners, copiers = equipment
+        try:
+            self.validate_equipment(printers, scanners, copiers )
+        except MyTypeError as err:
+            return err
+        except MyQuantityError as err:
+            return err
+        move_text = ' Moved:\n'
+        for i in range(int(printers)):
+            item = self.storage['Printer'].pop()
+            target_obj.storage['Printer'].append(item)
+            move_text += str(item)
+        for i in range(int(scanners)):
+            target_obj.storage['Scanner'].append(self.storage['Scanner'].pop())
+        for i in range(int(copiers)):
+            target_obj.storage['Copier'].append(self.storage['Copier'].pop())
+        return move_text
+    
+    # проверка вводимых данных
+    def validate_equipment(self, printers, scanners, copiers):
+        if not printers.isdigit() or not scanners.isdigit()  or not copiers.isdigit():
+            raise MyTypeError('количество должно быть числом')
+        if int(printers) > len(self.storage['Printer']) or int(scanners) > len(self.storage['Scanner']) or int(copiers)  > len(self.storage['Copier']):
+            raise MyQuantityError('такого количества нет на складе')
+
+    # Создание случайного принтера
     @staticmethod
     def random_printer():
         model = random.choice(['HP', 'Canon', 'Epson', 'Brother'])
@@ -44,14 +79,16 @@ class Storage:
         color = bool(random.getrandbits(1))
         return Printer(model, serial_number, cost, print_speed, color)
 
+    # Создание случайного сканера
     @staticmethod
-    def random_scaner():
+    def random_scanner():
         model = random.choice(['Mustek', 'Canon', 'Epson'])
         serial_number = f'sn{random.randint(100000, 999999)}'
         cost = random.randint(5000, 100000)
         scan_quality = random.choice([300, 600, 1200, 1400])
-        return Scaner(model, serial_number, cost, scan_quality)
+        return Scanner(model, serial_number, cost, scan_quality)
 
+    # Создание случайного ксерокса
     @staticmethod
     def random_copier():
         model = random.choice(['Xerox', 'Canon', 'HP', 'Brother'])
@@ -62,6 +99,7 @@ class Storage:
         return Copier(model, serial_number, cost, copy_speed, color)
 
 
+# дальше классы нужные по заданию, но почти не используемые. не успел))
 class OfficeEquipment:
     def __init__(self, model, serial_number, cost):
         self.model = model
@@ -79,7 +117,7 @@ class Printer(OfficeEquipment):
         return f'Принтер фирмы {self.model}, серийный номер {self.serial_number}'
 
 
-class Scaner(OfficeEquipment):
+class Scanner(OfficeEquipment):
     def __init__(self, model, serial_number, cost, scan_quality):
         super().__init__(model, serial_number, cost)
         self.scan_quality = scan_quality
@@ -99,46 +137,8 @@ class Copier(OfficeEquipment):
 
 
 if __name__ == '__main__':
-    # import random
-    # printers = []
-    # for i in range(1, 10):
-    #     model = random.choice(['HP', 'Canon', 'Epson', 'Brother'])
-    #     serial_number = f'sn{random.randint(100000, 999999)}'
-    #     cost = random.randint(5000, 100000)
-    #     print_speed = random.choice([10, 30, 50, 100])
-    #     color = bool(random.getrandbits(1))
-    #     printers.append(Printer(model, serial_number, cost, print_speed, color))
-    #
-    # scanners = []
-    # for i in range(1, 10):
-    #     model = random.choice(['Mustek', 'Canon', 'Epson'])
-    #     serial_number = f'sn{random.randint(100000, 999999)}'
-    #     cost = random.randint(5000, 100000)
-    #     scan_quality = random.choice([300, 600, 1200, 1400])
-    #     scanners.append(Scaner(model, serial_number, cost, scan_quality))
-    # scaner_epson = Scaner('Epson', 'sn4352', 5600, 1200)
-    #
-    # copiers = []
-    # for i in range(1, 10):
-    #     model = random.choice(['Xerox', 'Canon', 'HP', 'Brother'])
-    #     serial_number = f'sn{random.randint(100000, 999999)}'
-    #     cost = random.randint(5000, 100000)
-    #     copy_speed = random.choice([5, 10, 20])
-    #     color = bool(random.getrandbits(1))
-    #     copiers.append(Copier(model, serial_number, cost, copy_speed, color))
-
     main_storage = Storage()
-    # main_storage.accept(copiers[5])
-    # main_storage.accept(printers[5])
-    # main_storage.accept(printers[6])
-    print(main_storage)
-    print(main_storage.count())
+    office_storage = Storage()
 
-    storage = window.Window(main_storage)
-    storage.run()
-    # for i in printers:
-    #     print(i)
-    # for i in scanners:
-    #     print(i)
-    # for i in copiers:
-    #     print(i)
+    storage_project = Window(main_storage, office_storage)
+    storage_project.run()
--- window.py
+++ window.py
@@ -1,45 +1,143 @@
 import tkinter as tk
+from tkinter import messagebox
 
 
 class Window:
-
-    def __init__(self, main):
+    def __init__(self, main, office):
         self.root = tk.Tk()
         self.root.title = 'Storage'
         self.main = main
+        self.office = office
+        self.root.columnconfigure([0, 1], minsize=150)
 
-        self.main_storage = tk.Label(self.root, text="Главный склад")
-        self.main_storage_count = tk.Label(self.root, text=self.main.count())
-        self.printer_button = tk.Button(self.root, text='добавить принтер', command=self.printer_button)
-        self.scaner_button = tk.Button(self.root, text='добавить сканер', command=self.scaner_button)
-        self.copier_button = tk.Button(self.root, text='добавить ксерокс', command=self.copier_button)
+        self.frame_main = tk.Frame(self.root, relief=tk.RAISED, borderwidth=2)
+        self.main_storage = tk.Label(self.frame_main, text="Main storage")
+        self.main_storage_count = tk.Label(self.frame_main, text=self.main.count())
+        self.main_printer_button = tk.Button(self.frame_main, text='Add printer', command=self.main_printer_button)
+        self.main_scanner_button = tk.Button(self.frame_main, text='Add scanner', command=self.main_scanner_button)
+        self.main_copier_button = tk.Button(self.frame_main, text='Add copier', command=self.main_copier_button)
+        self.main_storage_all = tk.Label(self.frame_main, text='')
 
+        self.frame_main_move = tk.Frame(self.root, relief=tk.RAISED, borderwidth=2)
+        self.main_move = tk.Label(self.frame_main_move, text='Move to office')
+        self.main_printers_entry = tk.Entry(self.frame_main_move, width=5)
+        self.main_printers_entry.insert(0, '0')
+        self.main_move_printer = tk.Label(self.frame_main_move, text='printers')
+        self.main_scanners_entry = tk.Entry(self.frame_main_move, width=5)
+        self.main_scanners_entry.insert(0, '0')
+        self.main_move_scanners = tk.Label(self.frame_main_move, text='scanners')
+        self.main_copiers_entry = tk.Entry(self.frame_main_move, width=5)
+        self.main_copiers_entry.insert(0, '0')
+        self.main_move_copiers = tk.Label(self.frame_main_move, text='copiers')
+        self.main_move_button = tk.Button(self.frame_main_move, text ='move to office', command=self.move_button)
+
+        self.frame_office = tk.Frame(self.root, relief=tk.RAISED, borderwidth=2)
+        self.office_storage = tk.Label(self.frame_office, text="Office storage")
+        self.office_storage_count = tk.Label(self.frame_office, text=self.office.count())
+        self.office_storage_all = tk.Label(self.frame_office, text='')
+
+        self.frame_office_move = tk.Frame(self.root, relief=tk.RAISED, borderwidth=2)
+        self.office_move = tk.Label(self.frame_office_move, text='Return to main')
+        self.office_printers_entry = tk.Entry(self.frame_office_move, width=5)
+        self.office_printers_entry.insert(0, '0')
+        self.office_move_printer = tk.Label(self.frame_office_move, text='printers')
+        self.office_scanners_entry = tk.Entry(self.frame_office_move, width=5)
+        self.office_scanners_entry.insert(0, '0')
+        self.office_move_scanners = tk.Label(self.frame_office_move, text='scanners')
+        self.office_copiers_entry = tk.Entry(self.frame_office_move, width=5)
+        self.office_copiers_entry.insert(0, '0')
+        self.office_move_copiers = tk.Label(self.frame_office_move, text='copiers')
+        self.office_move_button = tk.Button(self.frame_office_move, text ='return to main', command=self.return_button)
+
+    # прорисовка всего
     def draw_widgets(self):
+        self.frame_main.grid(row=0, column=0, padx=20, pady=5, sticky="nwse")
         self.main_storage.grid(row=0, column=0)
         self.main_storage_count.grid(row=1, column=0)
-        self.printer_button.grid(row=2, column=0)
-        self.scaner_button.grid(row=3, column=0)
-        self.copier_button.grid(row=4, column=0)
+        self.main_printer_button.grid(row=2, column=0)
+        self.main_scanner_button.grid(row=3, column=0)
+        self.main_copier_button.grid(row=4, column=0)
+        self.main_storage_all.grid(row=5, column=0)
+       
+        self.frame_main_move.grid(row=1, column=0, padx=20, pady=5, sticky="nwse")
+        self.main_move.grid(row=0, column=0)
+        self.main_printers_entry.grid(row=1, column=0)
+        self.main_move_printer.grid(row=1, column=1)
+        self.main_scanners_entry.grid(row=2, column=0)
+        self.main_move_scanners.grid(row=2, column=1)
+        self.main_copiers_entry.grid(row=3, column=0)
+        self.main_move_copiers.grid(row=3, column=1)
+        self.main_move_button.grid(row=4, column=0)
 
+        self.frame_office.grid(row=0, column=1, padx=20, pady=5, sticky="nwse")
+        self.office_storage.grid(row=0, column=1)
+        self.office_storage_count.grid(row=1, column=1)
+        self.office_storage_all.grid(row=5, column=1)
+
+        self.frame_office_move.grid(row=1, column=1, padx=20, pady=5, sticky="nwse")
+        self.office_move.grid(row=0, column=0)
+        self.office_printers_entry.grid(row=1, column=0)
+        self.office_move_printer.grid(row=1, column=1)
+        self.office_scanners_entry.grid(row=2, column=0)
+        self.office_move_scanners.grid(row=2, column=1)
+        self.office_copiers_entry.grid(row=3, column=0)
+        self.office_move_copiers.grid(row=3, column=1)
+        self.office_move_button.grid(row=4, column=0)
+
+    # главный запуск
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
+        
+    # функции для кнопок
+    def main_printer_button(self):
+        self.main.random_accept('Printer')
+        self.main_update_storage()
 
-    def printer_button(self):
-        self.main.accept('Printer')
+    def main_scanner_button(self):
+        self.main.random_accept('Scanner')
+        self.main_update_storage()
+
+    def main_copier_button(self):
+        self.main.random_accept('Copier')
+        self.main_update_storage()
+
+    def office_printer_button(self):
+        self.office.random_accept('Printer')
+        self.office_update_storage()
+
+    def office_scanner_button(self):
+        self.office.random_accept('Scanner')
+        self.office_update_storage()
+
+    def office_copier_button(self):
+        self.office.random_accept('Copier')
+        self.office_update_storage()
+
+    def move_button(self):
+        quantity_printers = self.main_printers_entry.get()
+        quantity_scanners = self.main_scanners_entry.get()
+        quantity_copiers = self.main_copiers_entry.get()
+        result = self.main.move_to(self.office, quantity_printers, quantity_scanners, quantity_copiers)
+        self.main_update_storage()
+        self.office_update_storage()
+        messagebox.showinfo(title='Moved', message=result)
+            
+    def return_button(self):
+        quantity_printers = self.office_printers_entry.get()
+        quantity_scanners = self.office_scanners_entry.get()
+        quantity_copiers = self.office_copiers_entry.get()
+        result = self.office.move_to(self.main, quantity_printers, quantity_scanners, quantity_copiers)
+        self.main_update_storage()
+        self.office_update_storage()
+        messagebox.showinfo(title='Moved', message=result)
+
+            
+    # обновление после нажатия кнопок
+    def main_update_storage(self):
         self.main_storage_count.config(text=self.main.count())
-        self.root.update()
+        self.main_storage_all.config(text=f'Full list:\n {self.main}')
 
-    def scaner_button(self):
-        self.main.accept('Scaner')
-        self.main_storage_count.config(text=self.main.count())
-        self.root.update()
-
-    def copier_button(self):
-        self.main.accept('Copier')
-        self.main_storage_count.config(text=self.main.count())
-        self.root.update()
-
-# if __name__ == '__main__':
-#     window = Window()
-#     window.run()
+    def office_update_storage(self):
+        self.office_storage_count.config(text=self.office.count())
+        self.office_storage_all.config(text=f'Full list:\n {self.office}')
